dca655062d95af72bd3954ece9fd1f727f00ca9a
dwardo <benjamin.reveille@gmail.com>
2023-06-11 12:00:10 +0200

    Custom OTA server (using local hosting of https://github.com/julianxhokaxhiu/LineageOTA)
    Change-Id: I9d82df17326c7a3657617219f7b7bef8f10288e4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,7 @@ DEVICE_PATH := device/oneplus/fajita
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # inherit from the proprietary version
